eac13e09c211aa3947f6666f343567c4ce02db5d
wScottSh <scott.sheppard@hey.com>
2023-01-17 12:00:25 -0800

    finishes the kata

--- codewars-2023/202301170944.cpp
+++ codewars-2023/202301170944.cpp
@@ -11,36 +11,30 @@
 class GapInPrimes
 {
 public:
-    static int isPrime(int n) {
-        if (n <= 1) {
-            return 0;
+    static bool isPrime(long long n) { // create a function that checks if a number is prime
+        if (n <= 1) { // if the number is less than or equal to 1...
+            return false; // then it can't be prime
         }
-        for (int i = 2; i <= sqrt(n); i++) {
-            if (n % i == 0) {
-                return i;
+        for (long long i = 2; i <= sqrt(n); i++) { //starting at 2, iterate up to the square root of the provided number
+            if (n % i == 0) { // if the provided number is not evenly divisible by the iterated integer
+                return false; // it isn't prime
             }
         }
-        return 0;
+        return true; // if it passes this far, the remaining number is prime - return true
     }
     
     static std::pair<long long, long long> gap(int g, long long m, long long n) {
-        std::vector<int> primes;
-        int first = primes.at(0);
-        int second = primes.at(1);
-        
-        // iterate through the start and end numbers, finding the list of primes
-       for (long long i = m; i <= n; i++) {
-           if (isPrime(i)) primes.push_back(i);
-       }
-       
-       // take the first two primes in the list and check the gap
-       while (primes.size()) {
-           if (second - first == g) {
-               return std::pair {first, second};
-           } else {
-               primes.erase(primes.begin());
-           }
+        std::vector<long long> primes; // initialize a vector to hold the primes
+        for (long long i = m; i <= n; i++) { // iterate through the starting (m) and ending (n) numbers
+           if (isPrime(i)) primes.push_back(i); // push all the primes into `primes` vector
         }
+        
+        for (int i = 1; i < primes.size(); ++i) { // iterate through the primes vector
+            if (primes[i] - primes[i-1] == g) { // compare the gap between the first two numbers in the primes vector against the provided gap (g)
+                return std::make_pair(primes[i-1], primes[i]); // if it passes, return the first two elements as a pair
+            }
+        }
+        return std::make_pair(0,0); // otherwise default to returning (0,0)
     }
 };
-
+
