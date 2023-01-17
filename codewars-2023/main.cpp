60d0b0d12b1bb761f85310542df5d0ca01a3054d
wScottSh <scott.sheppard@hey.com>
2023-01-17 07:43:02 -0800

    Initial Commit

---
+++ codewars-2023/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  codewars-2023
+//
+//  Created by Scott Sheppard on 1/17/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
